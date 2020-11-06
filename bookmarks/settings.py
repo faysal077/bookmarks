2e2ee27b78b0bcb2a99bc549a9798ff805c58052
faysal077 <shadmanfaysal053@gmail.com>
2020-11-07 00:55:07 +0600

    Django 3 By Example

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#%t5&&be&izoi5(l3n@#(45i0#xzdcw-mx(d995^*6n5y03j-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['songsoptok.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['songsoptok.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
